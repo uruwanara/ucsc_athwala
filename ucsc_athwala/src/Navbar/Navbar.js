08ff2767d392437ffe32cb7ff37f3197b94f7e53
DanuAnjana <56577570+DanuAnjana@users.noreply.github.com>
2021-07-10 16:17:49 +0530

    create new navigation bar

--- ucsc_athwala/src/Navbar/Navbar.js
+++ ucsc_athwala/src/Navbar/Navbar.js
@@ -1,71 +1,27 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Toolbar from '@material-ui/core/Toolbar';
-import IconButton from '@material-ui/core/IconButton';
-import AccountCircle from '@material-ui/icons/AccountCircle';
-import MenuItem from '@material-ui/core/MenuItem';
-import Menu from '@material-ui/core/Menu';
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
-  title: {
-    flexGrow: 1,
-  },
-}));
+import '../css/navbar.css';
 
 export default function Navbar() {
-  const classes = useStyles();
-  const [anchorEl, setAnchorEl] = React.useState(null);
-  const open = Boolean(anchorEl);
-
-  const handleMenu = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
-
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
-
-  return (
-    <div className={classes.root}>
-
-        <Toolbar style={{ float: 'right'}}>
-            <div>
-              <IconButton
-                aria-label="account of current user"
-                aria-controls="menu-appbar"
-                aria-haspopup="true"
-                onClick={handleMenu}
-                color="inherit"
-              >
-                <AccountCircle />
-              </IconButton>
-              <Menu
-                id="menu-appbar"
-                anchorEl={anchorEl}
-                anchorOrigin={{
-                  vertical: 'top',
-                  horizontal: 'right',
-                }}
-                keepMounted
-                transformOrigin={{
-                  vertical: 'top',
-                  horizontal: 'right',
-                }}
-                open={open}
-                onClose={handleClose}
-              >
-                <MenuItem onClick={handleClose}>Profile</MenuItem>
-                <MenuItem onClick={handleClose}>My account</MenuItem>
-                <MenuItem onClick={handleClose}>LogOut</MenuItem>
-              </Menu>
-            </div>
-        </Toolbar>
-    </div>
-  );
+    return(
+        <div>
+           <nav className="navbar navbar-expand-sm justify-content-end navbar-custom">
+               <div><ul className="navbar-nav">
+                    
+                <li className="nav-item nav-item-custom">
+                <div className="dropdown" style={{float:"right"}}>
+                    <i className="fa fa-user-circle fa-2x" aria-hidden="true" style={{paddingRight:40,color:'white'}}></i>
+                    
+                    <div className="dropdown-content">
+                        <a href="#profile"><i class="fa fa-user" aria-hidden="true"></i> Profile</a>
+                        <a href="#logout"><i class="fa fa-sign-out" aria-hidden="true"></i> Sign out</a>
+                    </div>
+                </div>
+                </li>
+                
+                </ul></div>
+                
+            </nav> 
+        </div>
+        
+    );
 }
