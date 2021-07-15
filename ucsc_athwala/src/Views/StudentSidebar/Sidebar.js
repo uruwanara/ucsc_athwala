d46bd76a0ab379d42953668df1fb13d69dc07635
NimashaSupunpraba <49788577+NimashaSupunpraba@users.noreply.github.com>
2021-07-16 02:09:00 +0530

    fix 1.1

--- ucsc_athwala/src/Views/StudentSidebar/Sidebar.js
+++ ucsc_athwala/src/Views/StudentSidebar/Sidebar.js
@@ -16,7 +16,7 @@ export default function Side() {
         <Link to=""><i className="fa fa-flag" aria-hidden="true" style={{color:'#ff6411',padding:10}}></i> Fundrasing</Link>
         <Link to=""><i className="fa fa-handshake-o" aria-hidden="true" style={{color:'#ff6411',padding:10}}></i> Job oppertunity</Link>
         <Link to="/stdlrngrp"><i className="fa fa-video-camera" aria-hidden="true" style={{color:'#ff6411',padding:10}}></i> Learning Groups</Link>
-        <Link to=""><i className="fa fa-comments" aria-hidden="true" style={{color:'#ff6411',padding:10}}></i> Chat</Link>
+        <Link to="/Chat"><i className="fa fa-comments" aria-hidden="true" style={{color:'#ff6411',padding:10}}></i> Chat</Link>
         <Link to=""><i className="fa fa-list-alt" aria-hidden="true" style={{color:'#ff6411',padding:10}}></i> UCSC help stack</Link>
         <Link to=""><i className="fa fa-question-circle" aria-hidden="true" style={{color:'#ff6411',padding:10}}></i> QA support</Link>
         <Link to="/stdcntcoun"><i className="fa fa-video-camera" aria-hidden="true" style={{color:'#ff6411',padding:10}}></i> Contact Councellor</Link>
