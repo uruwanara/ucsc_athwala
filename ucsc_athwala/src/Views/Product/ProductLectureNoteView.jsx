9eaa2f14b617431b65ebdcebe592e78558c48bb0
NimashaSupunpraba <49788577+NimashaSupunpraba@users.noreply.github.com>
2021-07-16 04:08:03 +0530

    add dumi data product

--- ucsc_athwala/src/Views/Product/ProductLectureNoteView.jsx
+++ ucsc_athwala/src/Views/Product/ProductLectureNoteView.jsx
@@ -10,6 +10,7 @@ export default function ProductCases(){
       <th scope="col">Title</th>
       <th scope="col">Image</th>
       <th scope="col">Description</th>
+      <th scope="col">Price</th>
       <th scope="col">Posted Date</th>
       <th scope="col">View Details</th>
     </tr>
@@ -19,6 +20,7 @@ export default function ProductCases(){
       <td>DSA Lecture note</td>
       <td> <img src={Event} alt="cap" class="card-img-top" style={{height:70,width:70,alignSelf:'left',marginLeft:5}} ></img></td>
       <td>DSA 2 lecture note.</td>
+      <td>Rs : 100.00</td>
       <td>2021-07-11</td>
      
       <td><button type="button" class="btn tb-btn">View</button></td>
@@ -27,7 +29,8 @@ export default function ProductCases(){
       <td>SE Lecture note</td>
       <td> <img src={Event} alt="cap" class="card-img-top" style={{height:70,width:70,alignSelf:'left',marginLeft:5}} ></img></td>
       <td>Software engineering 2 lecture note.</td>
-      <td>2021-07-11</td>
+      <td>Rs : 500.00</td>
+      <td>2020-08-10</td>
       
       <td><button type="button" class="btn tb-btn">View</button></td>
     </tr>
@@ -35,7 +38,8 @@ export default function ProductCases(){
       <td>DSA Lecture note</td>
       <td> <img src={Event} alt="cap" class="card-img-top" style={{height:70,width:70,alignSelf:'left',marginLeft:5}} ></img></td>
       <td> DSA 2 lecture note.</td>
-      <td>2021-07-11</td>
+      <td>Rs : 600.00</td>
+      <td>2021-04-11</td>
      
       <td><button type="button" class="btn tb-btn">View</button></td>
     </tr>
@@ -43,7 +47,8 @@ export default function ProductCases(){
       <td>Database Lecture note</td>
       <td> <img src={Event} alt="cap" class="card-img-top" style={{height:70,width:70,alignSelf:'left',marginLeft:5}} ></img></td>
       <td>Database 1 lecture note.</td>
-      <td>2021-07-11</td>
+      <td>Rs : 400.00</td>
+      <td>2021-07-06</td>
      
       <td><button type="button" class="btn tb-btn">View</button></td>
     </tr>
@@ -51,7 +56,8 @@ export default function ProductCases(){
       <td>Database Lecture note</td>
       <td> <img src={Event} alt="cap" class="card-img-top" style={{height:70,width:70,alignSelf:'left',marginLeft:5}} ></img></td>
       <td>Database 2 lecture note.</td>
-      <td>2021-07-11</td>
+      <td>Rs : 200.00</td>
+      <td>2019-10-11</td>
       
       <td><button type="button" class="btn tb-btn">View</button></td>
     </tr>
@@ -59,15 +65,8 @@ export default function ProductCases(){
       <td>DSA Lecture note</td>
       <td> <img src={Event} alt="cap" class="card-img-top" style={{height:70,width:70,alignSelf:'left',marginLeft:5}} ></img></td>
       <td>DSA 1 lecture note.</td>
-      <td>2021-07-11</td>
-      
-      <td><button type="button" class="btn tb-btn">View</button></td>
-    </tr>
-    <tr>
-      <td>DSA Lecture note</td>
-      <td> <img src={Event} alt="cap" class="card-img-top" style={{height:70,width:70,alignSelf:'left',marginLeft:5}} ></img></td>
-      <td>DSA 3 lecture note.</td>
-      <td>2021-07-11</td>
+      <td>Rs : 50.00</td>
+      <td>2020-07-08</td>
       
       <td><button type="button" class="btn tb-btn">View</button></td>
     </tr>
