7bd1a49b4885be78ea70595e7ad4c669717140d5
DanuAnjana <56577570+DanuAnjana@users.noreply.github.com>
2021-07-15 14:30:43 +0530

    Request_Donation

---
+++ ucsc_athwala/src/Views/Donation/Request_Moneyform.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import './Donation.css';
+
+export default function RequestMoney(){
+    return(
+    <div>
+    <form style={{marginLeft:150}}>
+        <div style={{marginBottom:20 ,fontFamily:'Poppins, sans-serif'}}><h4>Request form</h4></div>
+
+        <div class="form-row">
+            <div class="form-group col-md-8">
+                <label for="title">Title*</label>
+                <input type="text" class="form-control" id="title" placeholder="title" required></input>
+            </div>
+        </div>
+        <div class="form-row">
+            <div class="form-group col-md-8">
+                <label for="description">Description*</label>
+                <input type="text" class="form-control" id="description" placeholder="decription" required></input>
+            </div>
+        </div>
+        <div class="form-row">
+            <div class="form-group col-md-8">
+                <label for="amount">Amount*</label>
+                <input type="Number" class="form-control" id="amount" placeholder="amount" required></input>
+            </div>
+        </div>
+        <div class="form-row">
+            <div class="form-group col-md-8">
+                <label for="date">Before (around)</label>
+                <input type="date" class="form-control" id="date" placeholder="date"></input>
+            </div>
+        </div>
+        <div class="form-row">
+            <div class="form-group col-md-8">
+                <label for="amount">Any special Note</label>
+                <input type="text" class="form-control" id="amount" placeholder="any note.." required></input>
+            </div>
+        </div>
+        <div class="form-group">
+            <div class="form-check ">
+            <input class="form-check-input" type="checkbox" id="gridCheck"></input>
+                <label class="form-check-label" for="gridCheck">
+                    I Don't need to show my identity to others.
+                </label>
+            </div>
+            <div class="form-check ">
+            <input class="form-check-input" type="checkbox" id="gridCheck"></input>
+                <label class="form-check-label" for="gridCheck">
+                    show this in public donation section
+                </label>
+            </div>
+        </div>
+       
+        <button type="submit" class="btn btn-custom">Request</button>
+    </form>
+
+    </div>
+    );
+}
+
