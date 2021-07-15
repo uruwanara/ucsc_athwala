35b0edc36fa4dae3af53fafdbf79359585169d2e
DanuAnjana <56577570+DanuAnjana@users.noreply.github.com>
2021-07-15 22:12:52 +0530

    routing for donation ui

--- ucsc_athwala/src/App.js
+++ ucsc_athwala/src/App.js
@@ -8,21 +8,37 @@ import {
 import Login from './Views/Signin/SignIn'
 import SignUp from './Views/SignUp/SignUp'
 import StdDashBoard from './Views/StudentDashboard/StudentDashboard'
-import Viewdonations from './Views/Donation/Donation_page'
-<<<<<<< Updated upstream
+
 import StdContactCounsellor from './Views/StdContactCounsellor/StdContactCounsellor'
 import StdLearningGroup from './Views/StdLearningGroups/StdLearningGroups'
-=======
+
 import ViewEleProduct from './Views/Product/ProductPageViewEles'
 import ViewNoteProduct from './Views/Product/ProductPageViewNotes'
 // import SellNoteProduct from './Views/Product/ProductPageSellNote'
+
+
+import Viewcauses from './Views/Donation/Donation_page'
+import RequestNote from './Views/Donation/Request_Note'
+import RequestCloth from './Views/Donation/Request_Cloth'
+import RequestDevice from './Views/Donation/Request_Device'
+import RequestMoney from './Views/Donation/Request_Money'
+import RequestOther from './Views/Donation/Request_Other'
+import ViewMyrequest from './Views/Donation/MyRequesting'
+import ViewNoteCause from './Views/Donation/View_Notecause'
+import ViewClothCause from './Views/Donation/View_Clothcause'
+import ViewDeviceCause from './Views/Donation/View_Devicecause'
+import ViewMoneyCause from './Views/Donation/View_Moneycause'
+import ViewOtherCause from './Views/Donation/View_Othercause'
+import ViewReceivedDonation from './Views/Donation/View_MyrequestingReceived'
+import ViewPendingDonation from './Views/Donation/View_Myrequesting'
+import ViewNotReceivedDonation from './Views/Donation/View_MyrequestingNotrecived'
 
 
 
 import SellNoteProduct from './Views/Product/ProductPageSellNote'
 import SellEleProduct from './Views/Product/ProductPageSellEle'
 
->>>>>>> Stashed changes
+
 
 function App() {
     return (
@@ -34,8 +50,8 @@ function App() {
                 <Route path="/login" exact>
                     <Login />
                 </Route>
-                <Route path="/viewdonations" exact>
-                    <Viewdonations />
+                <Route path="/viewcauses" exact>
+                    <Viewcauses />
                 </Route>
 
                 <Route path="/stddashboard" exact>
@@ -45,6 +61,62 @@ function App() {
                 <Route path="/stdcntcoun" exact>
                     <StdContactCounsellor/>
                 </Route>
+                <Route path="/requestNote" exact>
+                    <RequestNote />
+                </Route>
+
+                <Route path="/requestCloth" exact>
+                    <RequestCloth />
+                </Route>
+
+                <Route path="/requestDevice" exact>
+                    <RequestDevice />
+                </Route>
+
+                <Route path="/requestMoney" exact>
+                    <RequestMoney />
+                </Route>
+
+                <Route path="/requestOther" exact>
+                    <RequestOther />
+                </Route>
+
+                <Route path="/viewMyrequest" exact>
+                    <ViewMyrequest />
+                </Route>
+
+                <Route path="/viewNoteCause_details" exact>
+                    <ViewNoteCause />
+                </Route>
+
+                <Route path="/viewClothCause_details" exact>
+                    <ViewClothCause />
+                </Route>
+
+                <Route path="/viewDeviceCause_details" exact>
+                    <ViewDeviceCause />
+                </Route>
+
+                <Route path="/viewMoneyCause_details" exact>
+                    <ViewMoneyCause />
+                </Route>
+
+                <Route path="/viewOtherCause_details" exact>
+                    <ViewOtherCause />
+                </Route>
+
+                <Route path="/viewReceived_donation" exact>
+                    <ViewReceivedDonation />
+                </Route>
+
+                <Route path="/viewPending_donation" exact>
+                    <ViewPendingDonation />
+                </Route>
+
+                <Route path="/viewNotReceived_donation" exact>
+                    <ViewNotReceivedDonation />
+                </Route>
+
 
                 <Route path="/stdlrngrp" exact>
                 <StdLearningGroup />
