26d59cd4fa0328524197ba9ec03627bc66b92fd1
uruwanara <uruwanara@users.noreply.github.com>
2021-07-16 10:01:57 +0530

    bug fix

--- ucsc_athwala/src/Views/UnionSidebar/UnionStudentSidebar.jsx
+++ ucsc_athwala/src/Views/UnionSidebar/UnionStudentSidebar.jsx
@@ -20,6 +20,13 @@ export default function UnionStudentSidebar() {
             <Link to=""><i className="fa fa-question-circle" aria-hidden="true" style={{color:'#ff6411',padding:10}}></i> QA support</Link>
             <Link to="/stdcntcoun"><i className="fa fa-video-camera" aria-hidden="true" style={{color:'#ff6411',padding:10}}></i> Contact Councellor</Link>
 
+<<<<<<< Updated upstream
+=======
+            <Link to="/unionDashbord"> <i className="fa fa-tachometer" aria-hidden="true" style={{ color: '#ff6411', padding: 10 }}></i> Dashboard</Link>
+            <Link to="/unionviewcauses"> <i className="fa fa-users" aria-hidden="true" style={{ color: '#ff6411', padding: 10 }}></i> Donation</Link>
+            <Link to="/ViewEleProduct"><i className="fa fa-shopping-cart" aria-hidden="true" style={{ color: '#ff6411', padding: 10 }}></i> Product</Link>
+              <Link to="/univiewauction"> <i className="fa fa-gavel" aria-hidden="true" style={{ color: '#ff6411', padding: 10 }}></i> Auction</Link>
+>>>>>>> Stashed changes
 
 
 
