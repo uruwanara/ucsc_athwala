a67d03519e2ce00621635772a610ffff5bcf8a29
DanuAnjana <56577570+DanuAnjana@users.noreply.github.com>
2021-08-21 16:29:46 +0530

    update myrequesting file

--- ucsc_athwala/src/Views/Donation/MyRequesting.js
+++ ucsc_athwala/src/Views/Donation/MyRequesting.js
@@ -1,24 +1,16 @@
 import React from 'react';
-import './Donation.css';
-import Tab from './Donation_tab';
 import MyCause from './Donation_MyCauses';
-import Side from '../StudentSidebar/Sidebar';
-import Nav from '../Navbar/Navbar';
+
 
 export default function View_Allcause(){
+
     return(
         <div>
-            <div><Nav /></div>
-            <div style={{display:'flex'}}>
-               <Side />
-                <div className="container-fluid container-custom">
-                    <Tab />
-                    <div style={{marginTop:30}}>
-                      <MyCause />  
-                    </div>
-                    
-                </div>
-            </div>
+          <div >
+            <MyCause />  
+          </div>
+
         </div>
     );
+
 }
