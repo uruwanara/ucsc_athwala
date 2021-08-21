30bbc24ab5949fae68af341d753327212cf2de42
DanuAnjana <56577570+DanuAnjana@users.noreply.github.com>
2021-08-21 16:22:30 +0530

    Reformat view donations

--- ucsc_athwala/src/Views/Donation/View_Devicecause.js
+++ ucsc_athwala/src/Views/Donation/View_Devicecause.js
@@ -8,6 +8,7 @@ import Card from '@material-ui/core/Card';
 import CardMedia from '@material-ui/core/CardMedia';
 import CardContent from '@material-ui/core/CardContent';
 import  Button from '@material-ui/core/Button';
+import {DeviceDoneeDetails,Description} from './View_Casues';
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -77,58 +78,12 @@ export default function View_Clothcause(){
                                 />   
                             </Card>
                         </Grid>
-                        <Grid item xs={6}>
-                        <Card className={classes.card}>
-                                <CardContent >
-                                    <Typography variant="h3" className={classes.title}>
-                                    Mobile phone
-                                    </Typography>
-                                    <Typography variant="subtitle2" className={classes.title}>
-                                    I'm a first year student. i lost my mobile phone in the train yesterday.i'm not able to buy a new phone imediately.
-                                     if someone has a used phone please donate me.
-                                    </Typography>
-                                </CardContent>   
-                            </Card>
-                            
-                        </Grid>
+                        <Description />
                     </Grid>
+                    
 
                     <Grid container spacing={2} >
-                        <Grid item xs={6}>
-                            <Card className={classes.card}>
-                            <CardContent>
-                                    <Typography variant="h5" className={classes.title}>
-                                       Donee's details
-                                    </Typography>
-                                    <div style={{display:"flex"}}>
-                                        <div>
-                                            <Typography variant="subtitle2" className={classes.labelname}>Model of device </Typography> 
-                                        </div>
-                                        <div>
-                                            <Typography variant="subtitle2" className={classes.labelvalue}>Android</Typography> 
-                                        </div>  
-                                    </div>
-                                    <div style={{display:"flex"}}>
-                                        <div>
-                                            <Typography variant="subtitle2" className={classes.labelname}>Brand name </Typography> 
-                                        </div>
-                                        <div>
-                                            <Typography variant="subtitle2" className={classes.labelvalue}>Any</Typography> 
-                                        </div>  
-                                    </div>
-                                    <div style={{display:"flex"}}>
-                                        <div>
-                                            <Typography variant="subtitle2" className={classes.labelname}>Before around</Typography> 
-                                        </div>
-                                        <div>
-                                            <Typography variant="subtitle2" className={classes.labelvalue}>2021-10-13</Typography> 
-                                        </div>  
-                                    </div>
-                                    
-                                    
-                                </CardContent>    
-                            </Card>
-                        </Grid>
+                        <DeviceDoneeDetails />
                         <Grid item xs={6}>
                         <Card className={classes.card}>
                                 <CardContent>
--- ucsc_athwala/src/Views/Donation/View_Othercause.js
+++ ucsc_athwala/src/Views/Donation/View_Othercause.js
@@ -8,6 +8,7 @@ import Card from '@material-ui/core/Card';
 import CardMedia from '@material-ui/core/CardMedia';
 import CardContent from '@material-ui/core/CardContent';
 import  Button from '@material-ui/core/Button';
+import { OtherDoneeDetails,Description } from './View_Casues';
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -77,50 +78,13 @@ export default function View_Clothcause(){
                                 />   
                             </Card>
                         </Grid>
-                        <Grid item xs={6}>
-                        <Card className={classes.card}>
-                                <CardContent >
-                                    <Typography variant="h3" className={classes.title}>
-                                    help for brother's recovery
-                                    </Typography>
-                                    <Typography variant="subtitle2" className={classes.title}>
-                                    I,m a second year student. My brother got accident last month. now he is recovering. 
-                                    we need a wheel chair for him. if someone has a wheel chair and willing to donate it please donate for me.
-                                    </Typography>
-                                </CardContent>   
-                            </Card>
-                            
-                        </Grid>
+                        <Description />
                     </Grid>
 
+                    
+
                     <Grid container spacing={2} >
-                        <Grid item xs={6}>
-                            <Card className={classes.card}>
-                            <CardContent>
-                                    <Typography variant="h5" className={classes.title}>
-                                       Donee's details
-                                    </Typography>
-                                    <div style={{display:"flex"}}>
-                                        <div>
-                                            <Typography variant="subtitle2" className={classes.labelname}>Donate for  </Typography> 
-                                        </div>
-                                        <div>
-                                            <Typography variant="subtitle2" className={classes.labelvalue}>Brother's recovery</Typography> 
-                                        </div>  
-                                    </div>
-                                    <div style={{display:"flex"}}>
-                                        <div>
-                                            <Typography variant="subtitle2" className={classes.labelname}>Before around</Typography> 
-                                        </div>
-                                        <div>
-                                            <Typography variant="subtitle2" className={classes.labelvalue}>2021-10-13</Typography> 
-                                        </div>  
-                                    </div>
-                                    
-                                    
-                                </CardContent>    
-                            </Card>
-                        </Grid>
+                        <OtherDoneeDetails />
                         <Grid item xs={6}>
                         <Card className={classes.card}>
                                 <CardContent>
