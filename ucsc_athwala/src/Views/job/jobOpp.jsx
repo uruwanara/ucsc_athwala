958e456839ad18f71ea03bdcac208a517aca941e
DanuAnjana <56577570+DanuAnjana@users.noreply.github.com>
2021-08-26 14:13:38 +0530

    create job opportunity ui

--- ucsc_athwala/src/Views/job/jobOpp.jsx
+++ ucsc_athwala/src/Views/job/jobOpp.jsx
@@ -1,79 +1,95 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core';
+import { Typography } from '@material-ui/core';
+import { Container } from '@material-ui/core';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import WorkIcon from '@material-ui/icons/Work';
+import { Button } from '@material-ui/core';
+import {Link} from "react-router-dom"; 
 
+const useStyles = makeStyles({
+    title:{
+    color:"#546e7a",
+    marginTop:5,
+    marginBottom:20,
+    },
+    table: {
+        minWidth: 650,
+      },
+    TableHead: {
+        fontWeight: 'bold',
+        fontFamily:"Poppins, sans-serif",
+    },
+    button:{
+        textTransform: 'initial',
+        borderRadius: 15,
 
-export default function JobCases(){
+    }
+});
+
+function createData(title,company,description,date) {
+    return { title,company,description,date };
+  }
+  
+  const rows = [
+    createData('Senior Software Engineer', 'IFS', 'Lead/Senior Software Engineer - C#, .Net, Data Structures, Algorithm Development', '2021-06-11'),
+    createData('Software Engineer', 'ICT Agency of Sri Lanka', 'Software Engineer job from ICT Agency of Sri Lanka in Colombo, Sri Lanka', '2020-05-13'),
+    createData('Software Engineer (Java)', 'Hitachi Digital Payment Solutions Ltd', 'Hiring now for Software Engineer (Java) at Hitachi Digital Payment Solutions Ltd Apply today.', '2021-04-11'),
+    createData('SOFTWARE ENGINEER', 'Sysco Labs', '	SOFTWARE ENGINEER job from Sysco Labs in Colombo, Sri Lanka', '2021-07-11'),
+    createData('Software Engineer', '	WSO2', 'Software Engineer - Integration job from WSO2 in Colombo, Sri Lanka', '2021-07-11'),
+    createData('Senior Software Engineer', 'Wiley', 'Senior Software Engineer - Analytics Engineering', '2020-06-12'),
+    createData('Associate/Senior/Software Engineer', 'Wiley Sri Lanka', '	Salesforce - Associate/Senior/Software Engineer from Java or .Net background', '2021-05-11'),
+  ];
+
+export default function My_donation(){
+    const classes = useStyles();
+
     return(
-    <div>
-        <table class="table table-striped">
-  <thead>
-    <tr>
-      <th scope="col">Title</th>
-      <th scope="col">Company Name</th>
-      <th scope="col">Description</th>
-      <th scope="col">Posted Date</th>
-      <th scope="col">View Details</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <td>Senior Software Engineer</td>
-      <td>IFS</td>
-      <td>Lead/Senior Software Engineer - C#, .Net, Data Structures, Algorithm Development</td>
-      <td>2021-06-11</td>
-     
-      <td><button type="button" class="btn tb-btn">View</button></td>
-    </tr>
-    <tr>
-      <td>Software Engineer</td>
-      <td>ICT Agency of Sri Lanka</td>
-      <td>Software Engineer job from ICT Agency of Sri Lanka in Colombo, Sri Lanka</td>
-      <td>2020-05-13</td>
-      
-      <td><button type="button" class="btn tb-btn">View</button></td>
-    </tr>
-    <tr>
-      <td>Software Engineer (Java)</td>
-      <td>Hitachi Digital Payment Solutions Ltd</td>
-      <td> Hiring now for Software Engineer (Java) at Hitachi Digital Payment Solutions Ltd Apply today.</td>
-      <td>2021-04-11</td>
-     
-      <td><button type="button" class="btn tb-btn">View</button></td>
-    </tr>
-    <tr>
-      <td>SOFTWARE ENGINEER</td>
-      <td>Sysco Labs</td>
-      <td>SOFTWARE ENGINEER job from Sysco Labs in Colombo, Sri Lanka</td>
-      <td>2021-07-11</td>
-     
-      <td><button type="button" class="btn tb-btn">View</button></td>
-    </tr>
-    <tr>
-      <td>Software Engineer</td>
-      <td>WSO2</td>
-      <td>Software Engineer - Integration job from WSO2 in Colombo, Sri Lanka</td>
-      <td>2021-07-11</td>
-      
-      <td><button type="button" class="btn tb-btn">View</button></td>
-    </tr>
-    <tr>
-      <td>Senior Software Engineer</td>
-      <td>Wiley</td>
-      <td>Senior Software Engineer - Analytics Engineering</td>
-      <td>2020-06-12</td>
-      
-      <td><button type="button" class="btn tb-btn">View</button></td>
-    </tr>
-    <tr>
-      <td>Associate/Senior/Software Engineer</td>
-      <td>Wiley Sri Lanka</td>
-      <td>Salesforce - Associate/Senior/Software Engineer from Java or .Net background</td>
-      <td>2021-05-11</td>
-      
-      <td><button type="button" class="btn tb-btn">View</button></td>
-    </tr>
-  </tbody>
-</table>
+        <div>
+            <div><Typography variant="h5" className={classes.title}>Job Oppertunity</Typography></div>
 
-    </div>
+            <TableContainer component={Paper}>
+                <Table className={classes.table} aria-label="a dense table">
+                    <TableHead >
+                    <TableRow>
+                        <TableCell></TableCell>
+                        <TableCell className={classes.TableHead}>Title</TableCell>
+                        <TableCell className={classes.TableHead} align="center">Company name</TableCell>
+                        <TableCell className={classes.TableHead} align="center">Description</TableCell>
+                        <TableCell className={classes.TableHead} align="center">Posted date</TableCell>
+                        <TableCell className={classes.TableHead} align="center">View</TableCell>
+                    </TableRow>
+                    </TableHead>
+                    <TableBody>
+                    {rows.map((row) => (
+                        <TableRow key={row.name}>
+                            <TableCell component="th" scope="row"><WorkIcon color="secondary"/></TableCell> 
+                        <TableCell component="th" scope="row">{row.title}</TableCell>
+                        <TableCell align="left">{row.company}</TableCell>
+                        <TableCell align="left">{row.description}</TableCell>
+                        <TableCell align="left">{row.date}</TableCell>
+                        <TableCell align="left">
+                            <Link to ='/std/jobview'>
+                            <Button 
+                            variant="contained" 
+                            className={classes.button} 
+                            color="primary" 
+                            >View</Button></Link>
+                        </TableCell>
+                        </TableRow>
+                    ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+        
+        
+
+        </div>
     );
 }
--- ucsc_athwala/src/Views/job/jobPage.jsx
+++ ucsc_athwala/src/Views/job/jobPage.jsx
@@ -1,28 +1,13 @@
 import React from 'react';
 import './job.css';
-// import Tab from './ProductTabs';
-import Cause from './jobOpp';
-import Side from '../StudentSidebar/Sidebar';
-import Nav from '../Navbar/Navbar';
-import {Link, useHistory } from "react-router-dom";
+import JobList from './jobOpp';
+
 
 
 export default function StuJobDashboard(){
-    const history = useHistory();
     return(
         <div>
-            <div><Nav /></div>
-            <div style={{display:'flex'}}>
-               <Side />
-                <div className="container-fluid container-custom">
-                    {/* <Tab /> */}
-                    <div style={{marginTop:30}}>
-                       <Cause /> 
-                    </div>
-                    
-                </div>
-            </div>
-            
+            <JobList />     
         </div>
     );
 }
