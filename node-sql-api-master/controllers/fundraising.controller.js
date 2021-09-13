3d9a25196288f51a5a0befbfacbd228cec507429
AbisesWeerasekera <52200609+AbisesWeerasekera@users.noreply.github.com>
2021-09-13 22:15:30 +0530

    create fundraising controller

---
+++ node-sql-api-master/controllers/fundraising.controller.js
@@ -0,0 +1,18 @@
+const db = require("../models");
+const Connection = require("../dbConnection");
+
+//Create a fundraising
+
+exports.create = (req, res) => {
+    console.log(req.body);
+
+    const fundID = req.body.fundID;
+    const fundName = req.body.fundname;
+    const fundDescription = req.body.fundName;
+
+connection
+
+
+}
+
+
