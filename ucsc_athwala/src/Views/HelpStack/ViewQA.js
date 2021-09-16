115b82917bec47288c15dc0c156581854da0cfac
DanuAnjana <56577570+DanuAnjana@users.noreply.github.com>
2021-09-16 11:52:25 +0530

    connect helpstack front end to backend

--- ucsc_athwala/src/Views/HelpStack/ViewQA.js
+++ ucsc_athwala/src/Views/HelpStack/ViewQA.js
@@ -1,11 +1,13 @@
-import React,{useState} from 'react';
+import React,{useState,useEffect} from 'react';
 import { makeStyles } from '@material-ui/core';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import TextField from '@material-ui/core/TextField';
 import Divider from '@material-ui/core/Divider';
 import Button from '@material-ui/core/Button';
-import {Link} from 'react-router-dom'
+import {Link} from 'react-router-dom';
+import { useLocation } from 'react-router';
+import axios from "axios";
 
 const useStyles = makeStyles({
     title:{
@@ -36,16 +38,45 @@ export default function ViewQA(){
     const classes = useStyles();
 
     const[vote, setVote] = useState(0);
+    const search = useLocation().search;
+    const[title, setTitle] = useState("");
+    const[questionbody, setQuestionbody] = useState("");
+    const[mapset, setMap] = useState([]);
 
     function incrementVote(){
           setVote(prevVote => prevVote + 1);
     }
 
+    useEffect(() => {
+        const questionid = new URLSearchParams(search).get("id");
+        fetchQuestion(questionid);
+        fetchAnswers(questionid);
+    },[]);
+
+    const fetchQuestion = (questionid) => {
+        axios.get("http://localhost:5000/api/helpstacks/viewquestion", {
+        params: {questionid:questionid},
+        }).then((response) => {
+            console.log(response.data);
+            setTitle(response.data[0].title);
+            setQuestionbody(response.data[0].question);
+        })
+    };
+
+    const fetchAnswers = (questionid) => {
+        axios.get("http://localhost:5000/api/helpstacks/viewanswer", {
+        params: {questionid:questionid},
+        }).then((response) => {
+            console.log(response.data);
+            setMap(response.data);
+        })
+    };
+
     return(
         <div>
             <Grid container md={12} spacing={4}>
                     <Grid item md={12}>
-                        <div><Typography variant="h5" className={classes.title}>how can I get field and value from nested array and query them to find doc in mongodb?</Typography></div>
+                        <div><Typography variant="h5" className={classes.title}>{title}</Typography></div>
                     </Grid>
                     <Grid item md={2}>
                         <button onClick={incrementVote} className={classes.votebutton}>Vote</button>
@@ -57,29 +88,36 @@ export default function ViewQA(){
                             name="question"
                             variant="outlined"
                             id="question"
-                            value="Question"
-                            fullWidth
-                            multiline
-                            rows ={10}
-                            autoFocus                           
-                        />  
-                    </Grid>
-                    <Grid item md={2}>
-                    <Typography variant="h5" className={classes.title}>Answer 1</Typography> 
-                    </Grid>
-                    <Grid item md={10}>
-                        <TextField
-                            autoComplete="question"
-                            name="question"
-                            variant="outlined"
-                            id="question"
-                            value="Answer"
+                            value={questionbody}
                             fullWidth
                             multiline
                             rows ={5}
                             autoFocus                           
                         />  
-                    </Grid >
+                    </Grid>
+
+                    {mapset.map(answer => (  
+                        <>
+                         <Grid item md={2}>
+                         <Typography variant="h5" className={classes.title}>Answer</Typography> 
+                         </Grid>
+                         <Grid item md={10}>
+                             <TextField
+                                 autoComplete="question"
+                                 name="question"
+                                 variant="outlined"
+                                 id="question"
+                                 value={answer.answer}
+                                 fullWidth
+                                 multiline
+                                 rows ={6}
+                                 autoFocus                           
+                             />  
+                         </Grid >
+                         </>
+                    ))}
+                    
+                    
                     <Grid item md={12}>
                         <Link to ='/std/helpstack/default'>
                         <Typography variant="h5" className={classes.backlink}>Back to Top Questions</Typography>
