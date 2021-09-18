8b9dbd8afd58a02841b264c98c41bc4036746788
DanuAnjana <56577570+DanuAnjana@users.noreply.github.com>
2021-09-18 08:38:57 +0530

    add donation routes and get data from database

--- node-sql-api-master/controllers/donation.controller.js
+++ node-sql-api-master/controllers/donation.controller.js
@@ -1,8 +1,14 @@
 const db = require("../models");
 const connection = require("../dbConnection")
 
+
+// in donation table if active column = 1 then donation is active. if active column = 0 then donation is not active.
+// in donation table if status = 0 then donation is Not Received
+// in donation table if status = 1 then donation is Pending
+// in donation table if status = 2 then donation is Received
+
 exports.viewall = (req, res) => {
-    connection.query('Select * from donations;',
+    connection.query('Select * from donations where active = 1 and status = 0;',
     (err, result,fields) => {
         if (err) {
             res.send(err);
@@ -262,6 +268,119 @@ exports.otherRequest = (req,res) => {
     }
     );
 };
+
+exports.search = (req, res) => {
+    const message = req.body.search; 
+
+    connection.query("Select * from donations where (title "+ "like'%"+ message+"%'"+" or description "+"like'%"+ message+"%')" + " AND active = 1 and status = 0;",
+    (err, result,fields) => { 
+        if (err) {
+            res.send(err);
+        } else {
+            res.send(result);
+        }
+    }
+);
+};
+
+exports.contact = (req, res) => {
+    const donationID = req.body.donationID;
+    const userID = req.body.userID;
+    const type = req.body.type;
+    const tel = req.body.tel;
+    const address = req.body.address;
+
+    connection.query("UPDATE donations SET status = 1 where donationID = ?",
+    [donationID],
+    (err, result,fields) => { 
+        if (err) {
+            res.send(err);
+        } else {
+
+            connection.query("UPDATE ?? SET status = 'Pending' where donation_id = ?",
+            [type,donationID],
+            (err1, result,fields) => { 
+                if (err) {
+                    res.send(err1);
+                } else {
+
+                    connection.query("insert into doners (donation_id,doner_id,tel,address) values(?,?,?,?);",
+                    [donationID,userID,tel,address],
+                    (err2, result,fields) => { 
+                        if (err) {
+                            res.send(err2);
+                        } else {
+                            res.send("success");
+                        }
+                    }
+                    );
+                    
+                }
+            }
+            );
+            
+        }
+    }
+    );
+};
+
+exports.donerdetails = (req, res) => {
+        const donationid = req.body.donationid;
+
+    connection.query("select users.fname , users.lname , doners.donate_at , doners.tel, doners.address FROM users INNER JOIN doners ON users.id = doners.doner_id WHERE doners.donation_id = ?",
+    [donationid],
+    (err, result,fields) => { 
+        if (err) {
+            res.send(err);
+        } else {
+            res.send(result);
+        }
+    }
+    );
+
+}
+
+exports.markstatus = (req, res) => {
+    const donationid = req.body.donationID;
+    const type = req.body.type;
+
+connection.query("update donations set status = 2 where donationID = ?",
+[donationid],
+(err, result,fields) => { 
+    if (err) {
+        res.send(err);
+    } else {
+        connection.query("update ?? set status = 'Received' where donation_id = ?",
+            [type,donationid],
+            (err1, result,fields) => { 
+                if (err1) {
+                    res.send(err1);
+                } else {
+                    res.send("success");
+                }
+            }
+        );
+    }
+}
+);
+
+}
+
+exports.myhistory = (req, res) => {
+    const userid = req.query.id;
+
+connection.query("select donations.donationID , donations.donationType , donations.description , donations.createdAt , donations.status, doners.donate_at FROM donations INNER JOIN doners ON donations.donationID = doners.donation_id WHERE doners.doner_id = ?",
+[userid],
+(err, result,fields) => { 
+    if (err) {
+        res.send(err);
+    } else {
+        res.send(result);
+    }
+}
+);
+
+}
 
 
 
